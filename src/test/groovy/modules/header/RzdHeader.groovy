85f45ca500b85809aae1a0c0496bf32054196d15
Aleksandr Alifirov <aalifirov@gmail.com>
2019-01-30 06:08:19 +0700

    all headers links

--- src/test/groovy/modules/header/RzdHeader.groovy
+++ src/test/groovy/modules/header/RzdHeader.groovy
@@ -9,12 +9,16 @@ class RzdHeader extends RzdModule {
 
         def context = "//div[@id='headMenu']"
 
+        АнглийскаяВерсия { $(By.xpath('//a[@href="http://eng.rzd.ru"]')) }
         МобильнаяВерсия { $(By.xpath("${context}//a[contains(@href, 'http://m.rzd.ru')]")) }
         ВерсияДляСВ { $(By.xpath("${context}//a[@title='Версия для слабовидящих']")) }
-        КартаСайта { $(By.xpath("${context}//a[@title='Карта сайта']")) }
+        КартаСайта { $(By.xpath("${context}//a[3]")) }
         ГлавнаяСтраница { $(By.xpath("${context}//a[@title='Главная страница']")) }
         ОтправитьПисьмо { $(By.xpath("${context}//a[@title='Отправить письмо']")) }
-
+        RSS { $(By.xpath("${context}//a[6]")) }
+        Все_сайты {$(By.xpath("${context}//a[contains(text(), 'Все сайты')]")) }
+        Регистрация { $(By.xpath("//a[contains(text(), 'Регистрация')]")) }
+        Вход { $(By.xpath("//a[contains(text(), 'Вход')]")) }
         О_Компании { $("a[href*='://']", text: 'О компании' )}
         Пресс_Центр { $(By.xpath("//a[contains(text(), 'Пресс-центр')]")) }
         Инвесторам { $(By.xpath("//a[contains(text(), 'Инвесторам')]")) }
@@ -23,7 +27,7 @@ class RzdHeader extends RzdModule {
         Пассажирам { $(By.xpath("${context}//a[text()='Пассажирам']")) }
         ГрузовыеПеревозки { $(By.xpath("${context}//a[text()='Грузовые перевозки']")) }
         РаботаВРЖД { $(By.xpath("${context}//a[text()='Работа в РЖД']")) }
-        Моложежный { $(By.xpath("${context}//a[text()='Молодежный']")) }
+        Молодежный { $(By.xpath("${context}//a[text()='Молодежный']")) }
     }
 
 }

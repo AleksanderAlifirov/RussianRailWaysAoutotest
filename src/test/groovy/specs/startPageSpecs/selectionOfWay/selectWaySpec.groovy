286d31a814a8e66e84f348ad079e99bd70e6b277
Aleksandr Alifirov <aalifirov@gmail.com>
2019-02-22 05:48:56 +0700

    Input departure and destination

--- src/test/groovy/specs/startPageSpecs/selectionOfWay/selectWaySpec.groovy
+++ src/test/groovy/specs/startPageSpecs/selectionOfWay/selectWaySpec.groovy
@@ -10,13 +10,30 @@ import spock.lang.Title
 class selectWaySpec extends RzdSpec {
     def "Type the departure station"(){
         given:
+        String departure = 'МОСКВА'
+        String destination = 'САНКТ-ПЕТЕРБУРГ'
         openStartPage()
         def view = at RzdStartPage
-        assert view.Left_GrayBlock.Passengers.Field_Departure.displayed
-        println "   Поле 'Откуда' присутствует."
-        assert view.Left_GrayBlock.Passengers.Field_Destination.displayed
-        println "   Поле 'Куда' присутствует."
-        view.Left_GrayBlock.Passengers.Field_Departure.value('Москва')
-
+        view.Left_GrayBlock.Passengers.Field_Date.displayed
+        /*assert view.Left_GrayBlock.Passengers.listOfDeparturesIsDisplayed()
+        assert view.Left_GrayBlock.Passengers.getListOfDepartures()
+        println "   Список станций 'Откуда' не пустой."
+        view.Left_GrayBlock.Passengers.Field_Departure.value("")
+        println "   Поле 'Откуда' очищено"
+        assert view.Left_GrayBlock.Passengers.listOfDestinationIsDisplayed()
+        assert view.Left_GrayBlock.Passengers.getListOfDestinations()
+        println "   Список станций 'Куда' не пустой."
+        view.Left_GrayBlock.Passengers.Field_Destination.value("")
+        println "   Поле 'Куда' очищено"
+        view.Left_GrayBlock.Passengers.selectDepartureStationByClick(departure)
+        println "   В поле 'Откуда' выбрана станция $departure"
+        view.Left_GrayBlock.Passengers.selectDestinationStationByClick(destination)
+        println "   В поле 'Куда' выбрана станция $destination"
+        view.Left_GrayBlock.Passengers.Field_Departure.value("")
+        println "   Поле 'Откуда' очищено"
+        view.Left_GrayBlock.Passengers.Field_Destination.value("")
+        println "   Поле 'Куда' очищено"
+        view.Left_GrayBlock.Passengers.selectDepartureStationByKeyPressing(departure)
+        view.Left_GrayBlock.Passengers.selectDestinationStationByKeyPressing(destination)*/
     }
 }
